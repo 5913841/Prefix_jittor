0965e80e9227fd186bf260d2f14b87da12ee1f5c
杜世博 <dsb20@mails.tsinghua.edu.cn>
2022-12-23 00:55:58 +0800

    完成了训练脚本

---
+++ gpt2/train_e2e.sh
@@ -0,0 +1,36 @@
+python3 run_language_modeling.py --output_dir=save_e2e_models_convcheck/data2textprefixtune_y_5_act_cat_b=5-e=5_d=0.0_u=no_lr=5e-05_w=0.0_s=101_r=n_m=512_o=1_o=1 \
+                --model_type=gpt2 \
+                --model_name_or_path=pretrained/gpt2-medium \
+                --tokenizer_name=gpt2-medium \
+                --per_device_train_batch_size 5 \
+                --per_device_eval_batch_size 5 \
+                --save_steps 100 \
+                --num_train_epochs 10 \
+                --do_train \
+                --train_data_file=../data/e2e_data/src1_train.txt \
+                --do_eval \
+                --line_by_line \
+                --save_total_limit 1 \
+                --overwrite_output_dir \
+                --task_mode data2text \
+                --eval_data_file=../data/e2e_data/src1_valid.txt \
+                --tuning_mode prefixtune \
+                --logging_dir save_e2e_models_convcheck/runs/data2textprefixtune_y_5_act_cat_b=5-e=5_d=0.0_u=no_lr=5e-05_w=0.0_s=101_r=n_m=512_o=1_o=1 \
+                --train_embs no \
+                --optim_prefix yes \
+                --preseqlen 5 \
+                --prefix_mode activation \
+                --format_mode cat \
+                --gradient_accumulation_steps 1 \
+                --learning_rate 5e-05 \
+                --weight_decay 0.0 \
+                --seed 101 \
+                --disable_tqdm \
+                --mid_dim 512 \
+                --init_random no \
+                --use_dropout no \
+                --prefix_dropout 0.0 \
+                --objective_mode 1 \
+                --evaluate_during_training \
+                --eval_steps 100  \
+                --cache_dir cache/gpt2-medium-e2e 
